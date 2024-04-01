f863620d3c1fc811321a2633c672a298b23a87f4
tuansydau <tukauu@gmail.com>
2024-04-01 16:59:09 -0400

    add: required styles and example classes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import UniverSheet from "./components/UniverSheet";
+import { DEFAULT_WORKBOOK_DATA } from "./assets/default-workbook-data";
 
 function App() {
+  const [data] = useState(DEFAULT_WORKBOOK_DATA);
+  const univerRef = useRef();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="root">
+      <div style={{ display: "flex", flexDirection: "column", height: "100%" }}>
+        <div className="bar">
+          <button
+            onClick={() => {
+              console.log(univerRef.current?.getData());
+            }}
+          >
+            Get Data
+          </button>
+        </div>
+        <UniverSheet style={{ flex: 1 }} ref={univerRef} data={data} />
+      </div>
     </div>
   );
 }
